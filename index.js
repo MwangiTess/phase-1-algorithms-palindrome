79645cf065492e2c2c12109125570643fb330a46
Teresia <teresiamwangi26@gmail.com>
2023-04-01 16:22:16 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(data) {
+  let word = data.split('').reverse().join('');
+  
+  if (data == word) {
+    return true;
+  } else {
+    return false;
+  };
 }
+
+isPalindrome("dad");
+
+console.log(isPalindrome("abba")); 
+console.log(isPalindrome('racecar'));
+console.log(isPalindrome(`'a' "`));
+console.log(isPalindrome('robot')); 
+console.log(isPalindrome(`'ab' "`));
+
+
+
+
+
+  /*Write your algorithm here
+   Writing a function that returns true or false if the input is really a Pailindrome like madam or refer
+  if the input is Madam the out put should be true and if input is furaha the output should be false */
+
 
 /* 
   Add your pseudocode here
+   create a variable called word, using the inbuilt method split it which will split a string into a substring eg true will be t r u e or m o m
+  Then using reverse() it will reverse the order of elements of the word eg: t r u e will be e u r t or m o m
+   After reversing we join the word back together it will be eurt or mom if  the word is a palindrome using the if conditional will return true or false
+
 */
 
 /*
   Add written explanation of your solution here
+  Incorporating the if...else conditional will return true or false for the argument passed in our function.
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -22,4 +51,6 @@ if (require.main === module) {
   console.log("=>", isPalindrome("robot"));
 }
 
-module.exports = isPalindrome;
+module.exports = isPalindrome; 
+
+
